04ff215d976774589217088b457b56b6e3fd554c
caicheng <251329204@qq.com>
2018-04-01 15:53:40 +0800

    v5 push

---
+++ Plugin.php
@@ -0,0 +1,251 @@
+<?php
+/**
+ * cosV5--for Typecho 使用cosV5版本sdk
+ *
+ * @package cosUploadV5
+ * @author 菜菜子
+ * @version 3.0
+ * @link https://www.cc430.cn/index.php/archives/458/
+ * @date 2018.4.1
+ */
+require(__DIR__ . DIRECTORY_SEPARATOR . 'cos-autoloader.php');
+
+class cosUploadV5_Plugin implements Typecho_Plugin_Interface
+{
+    /**
+     * 激活插件方法,如果激活失败,直接抛出异常
+     *
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function activate()
+    {
+        //上传
+        Typecho_Plugin::factory('Widget_Upload')->uploadHandle = array('cosUploadV5_Plugin', 'uploadHandle');
+        //修改
+        Typecho_Plugin::factory('Widget_Upload')->modifyHandle = array('cosUploadV5_Plugin', 'modifyHandle');
+        //删除
+        Typecho_Plugin::factory('Widget_Upload')->deleteHandle = array('cosUploadV5_Plugin', 'deleteHandle');
+        //路径参数处理
+        Typecho_Plugin::factory('Widget_Upload')->attachmentHandle = array('cosUploadV5_Plugin', 'attachmentHandle');
+        //文件内容数据
+        Typecho_Plugin::factory('Widget_Upload')->attachmentDataHandle = array('cosUploadV5_Plugin', 'attachmentDataHandle');
+    }
+
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     *
+     * @static
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function deactivate()
+    {
+    }
+
+    public static function init($options)
+    {
+        return new Qcloud\Cos\Client(array('region' => $options->region,
+            'credentials' => array(
+                'appId' => $options->appid,
+                'secretId' => $options->ak,
+                'secretKey' => $options->sk)));
+    }
+
+    /**
+     * 获取插件配置面板
+     *
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+    public static function config(Typecho_Widget_Helper_Form $form)
+    {
+        $appid = new Typecho_Widget_Helper_Form_Element_Text('appid',
+            null, '',
+            _t('APPID：'),
+            _t('<a href="https://console.qcloud.com/cam/capi" target="_blank">获取APPID</a>'));
+        $form->addInput($appid->addRule('required', _t('APPID 不能为空！')));
+
+        $ak = new Typecho_Widget_Helper_Form_Element_Text('ak',
+            NULL, '',
+            _t('SecretId：'),
+            _t('<a href="https://console.qcloud.com/cam/capi" target="_blank">获取SecretId</a>'));
+        $form->addInput($ak->addRule('required', _t('SecretId不能为空！')));
+
+        $sk = new Typecho_Widget_Helper_Form_Element_Text('sk',
+            NULL, '',
+            _t('SecretKey：'),
+            _t('<a href="https://console.qcloud.com/cam/capi" target="_blank">获取SecretKey</a>'));
+        $form->addInput($sk->addRule('required', _t('SecretKey不能为空！')));
+
+        $region = new Typecho_Widget_Helper_Form_Element_Radio('region',
+            array(
+                'ap-beijing-1' => _t('北京一区（华北）'),
+                'ap-beijing	' => _t('北京'),
+                'ap-shanghai' => _t('上海（华东）'),
+                'ap-guangzhou' => _t('广州（华南）'),
+                'ap-chengdu' => _t('成都（西南）'),
+                'ap-chongqing' => _t('重庆'),
+                'ap-singapore' => _t('新加坡'),
+                'ap-hongkong' => _t('香港'),
+                'na-toronto' => _t('多伦多'),
+                'eu-frankfurt' => _t('法兰克福'),
+                'ap-mumbai' => _t('孟买'),
+                'ap-seoul' => _t('首尔'),
+                'na-siliconvalley' => _t('硅谷'),
+                'na-ashburn' => _t('弗吉尼亚'),
+            ),
+            'ap-guangzhou',
+            _t('选择bucket节点')
+        );
+        $form->addInput($region->addRule('required', _t('bucket节点不能为空！')));
+
+
+        $bucketName = new Typecho_Widget_Helper_Form_Element_Text('bucket',
+            NULL, 'bucketName',
+            _t('Bucket名称：'),
+            _t('例如bucket-1'));
+        $form->addInput($bucketName->addRule('required', _t('Bucket名称不能为空！')));
+
+
+        $domain = new Typecho_Widget_Helper_Form_Element_Text('domain',
+            NULL, 'http://',
+            _t('使用的域名,必填,请带上http://或https://，可使用默认域名或自定义域名'),
+            _t('在bucket中的域名管理<br>默认域名形如：http://bucket-1-1252063625.cos.ap-chengdu.myqcloud.com<br>自定义域名形如：https://cos.cc430.cn'));
+        $form->addInput($domain->addRule('required', _t('cos域名不能为空！')));
+    }
+
+    /**
+     * 个人用户的配置面板
+     *
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form
+     * @return void
+     */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form)
+    {
+    }
+
+    /**
+     * 上传文件处理函数
+     *
+     * @access public
+     * @param array $file 上传的文件
+     * @return mixed
+     */
+    public static function uploadHandle($file)
+    {
+        if (empty($file['name'])) return false;
+        //获取扩展名
+        $ext = self::getSafeName($file['name']);
+        //判定是否是允许的文件类型
+        if (!Widget_Upload::checkFileType($ext)) return false;
+
+        //获取文件名
+        $filePath = '/' . date('Y') . '/' . date('m') . '/' . date('d') . '/';
+        $fileName = time() . '.' . $ext;
+        //cos上传文件的路径+名称
+        $newPath=$filePath.$fileName;
+        //如果没有临时文件，则退出
+        if (!isset($file['tmp_name'])) return false;
+        //获取插件参数
+        $options = Typecho_Widget::widget('Widget_Options')->plugin('cosUploadV5');
+
+        //初始化cos
+        $cos_object = self::init($options);
+        $srcPath = $file['tmp_name'];
+        $handle = fopen($srcPath, "r");
+        $contents = fread($handle, $file['size']);//获取二进制数据流
+        fclose($handle);
+        $cos_object->upload($options->bucket, $newPath, $contents);
+
+        return array(
+            'name' => $file['name'],
+            'path' => $newPath,
+            'size' => $file['size'],
+            'type' => $ext,
+            'mime' => @Typecho_Common::mimeContentType(rtrim($options->domain, '/') . '/' .  $newPath),
+        );
+    }
+
+    /*
+     * 初始化bucket位置
+     */
+    public static function region($option)
+    {
+        $region = $option->region;
+        return $region;
+    }
+
+
+    /**
+     * 文件删除
+     *
+     * @access public
+     * @param array $content 当前文件信息
+     * @return mixed
+     */
+    public static function deleteHandle($content)
+    {
+        $options = Typecho_Widget::widget('Widget_Options')->plugin('cosUploadV5');
+        $cos_object = self::init($options);
+
+        $err = $cos_object->deleteObject(array(
+            'Bucket' => $options->bucket,
+            'Key' => $content['attachment']->path)
+        );
+        return !$err;
+    }
+
+    /**
+     * 获取实际文件数据
+     *
+     * @access public
+     * @param array $content
+     * @return string
+     */
+    public static function attachmentDataHandle($content)
+    {
+        $options = Typecho_Widget::widget('Widget_Options')->plugin('cosUploadV5');
+        $cos_object = self::init($options);
+        list($ret, $err) = $cos_object->stat($options->bucket, $content['attachment']);
+        return $err === null ? $ret : false;
+    }
+
+    /**
+     * 获取实际文件绝对访问路径
+     *
+     * @access public
+     * @param array $content 文件相关信息
+     * @return string
+     */
+    public static function attachmentHandle(array $content)
+    {
+        $domain = Typecho_Widget::widget('Widget_Options')->plugin('cosUploadV5')->domain;
+        $tmp = preg_match('/http(s)?:\/\/[\w\d\.\-\/]+$/is', $domain);    //粗略验证域名
+        if (!$tmp) return false;
+        return Typecho_Common::url($content['attachment']->path, $domain);
+    }
+
+
+    /**
+     * 获取安全的文件名
+     *
+     * @param string $name
+     * @static
+     * @access private
+     * @return string
+     */
+    private static function getSafeName(&$name)
+    {
+        $name = str_replace(array('"', '<', '>'), '', $name);
+        $name = str_replace('\\', '/', $name);
+        $name = false === strpos($name, '/') ? ('a' . $name) : str_replace('/', '/a', $name);
+        $info = pathinfo($name);
+        $name = substr($info['basename'], 1);
+        return isset($info['extension']) ? strtolower($info['extension']) : '';
+    }
+}
---
+++ sample.php
@@ -0,0 +1,428 @@
+<?php
+
+require(__DIR__ . DIRECTORY_SEPARATOR . 'cos-autoloader.php');
+
+$cosClient = new Qcloud\Cos\Client(array('region' => getenv('COS_REGION'),
+    'credentials'=> array(
+        'appId' => getenv('COS_APPID'),
+        'secretId'    => getenv('COS_KEY'),
+        'secretKey' => getenv('COS_SECRET'))));
+#listBuckets
+try {
+    $result = $cosClient->listBuckets();
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+
+#createBucket
+try {
+    $result = $cosClient->createBucket(array('Bucket' => 'testbucket-1252448703'));
+    print_r($result);
+    } catch (\Exception $e) {
+    echo "$e\n";
+}
+
+
+#uploadbigfile
+try {
+    $result = $cosClient->upload(
+        $bucket='testbucket-1252448703',
+        $key = '111.txt',
+        $body = str_repeat('a', 5* 1024 * 1024),
+        $options = array(
+            "ACL"=>'private',
+            'CacheControl' => 'private',
+            'ServerSideEncryption' => 'AES256'));
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+
+#putObject
+try {
+    $result = $cosClient->putObject(array(
+        'Bucket' => 'testbucket-1252448703',
+        'Key' => '11//32//43',
+        'Body' => 'Hello World!',
+        'ServerSideEncryption' => 'AES256'));
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+
+#putBucketVersioning
+try {
+    $result = $cosClient->putBucketVersioning(
+    array('Bucket' => 'lewzylu02',
+    'Status' => 'Enabled')
+    );
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+
+#getObject
+try {
+    $result = $cosClient->getObject(array(
+        'Bucket' => 'testbucket-1252448703',
+        'Key' => '11',
+        'VersionId' =>'111'));
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+#getBucketLocation
+try {
+    $result = $cosClient->getBucketLocation(array(
+    'Bucket' => 'lewzylu02',
+    ));
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+;
+
+#deleteObject
+try {
+    $result = $cosClient->deleteObject(array(
+        'Bucket' => 'testbucket-1252448703',
+        'Key' => '111.txt',
+        'VersionId' => 'string'));
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+
+#deleteObjects
+try {
+    $result = $cosClient->deleteObjects(array(
+        // Bucket is required
+        'Bucket' => 'string',
+        // Objects is required
+        'Objects' => array(
+            array(
+                // Key is required
+                'Key' => 'string',
+                'VersionId' => 'string',
+            ),
+            // ... repeated
+        ),
+    ));
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+
+#deleteBucket
+try {
+    $result = $cosClient->deleteBucket(array(
+        'Bucket' => 'testbucket-1252448703'));
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+
+#headObject
+try {
+    $result = $cosClient->headObject(array(
+        'Bucket' => 'testbucket-1252448703',
+        'Key' => '11',
+        'VersionId' =>'111',
+        'ServerSideEncryption' => 'AES256'));
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+#headBucket
+try {
+    $result = $cosClient->headBucket(array(
+        'Bucket' => 'testbucket-1252448703'));
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+
+#listObjects
+try {
+    $result = $cosClient->headObject(array(
+        'Bucket' => 'testbucket-1252448703',
+        'Key' => '11'));
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+#ListObjectVersions
+try {
+    $result = $cosClient->ListObjectVersions(
+        array('Bucket' => 'lewzylu02',
+            'Prefix'=>'test1G')
+    );
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+
+#listObjects
+try {
+    $result = $cosClient->listObjects(array(
+        'Bucket' => 'testbucket-1252448703'));
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+#getBucketVersioning
+try {
+    $result = $cosClient->getBucketVersioning(
+        array('Bucket' => 'lewzylu02',)
+    );
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+
+#getObjectUrl
+try {
+    $bucket =  'testbucket-1252448703';
+    $key = 'hello.txt';
+    $url = "/{$key}";
+    $request = $cosClient->get($url);
+    $signedUrl = $cosClient->getObjectUrl($bucket, $key, '+10 minutes');
+    echo ($signedUrl);
+
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+
+#putBucketACL
+try {
+    $result = $cosClient->PutBucketAcl(array(
+        'Bucket' => 'testbucket-1252448703',
+        'Grants' => array(
+            array(
+                'Grantee' => array(
+                    'DisplayName' => 'qcs::cam::uin/327874225:uin/327874225',
+                    'ID' => 'qcs::cam::uin/327874225:uin/327874225',
+                    'Type' => 'CanonicalUser',
+                ),
+                'Permission' => 'FULL_CONTROL',
+            ),
+            // ... repeated
+        ),
+        'Owner' => array(
+            'DisplayName' => 'qcs::cam::uin/3210232098:uin/3210232098',
+            'ID' => 'qcs::cam::uin/3210232098:uin/3210232098',
+        ),));
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+
+#getBucketACL
+try {
+    $result = $cosClient->GetBucketAcl(array(
+        'Bucket' => 'testbucket-1252448703',));
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+
+#putObjectACL
+try {
+    $result = $cosClient->PutBucketAcl(array(
+        'Bucket' => 'testbucket-1252448703',
+        'Grants' => array(
+            array(
+                'Grantee' => array(
+                    'DisplayName' => 'qcs::cam::uin/327874225:uin/327874225',
+                    'ID' => 'qcs::cam::uin/327874225:uin/327874225',
+                    'Type' => 'CanonicalUser',
+                ),
+                'Permission' => 'FULL_CONTROL',
+            ),
+            // ... repeated
+        ),
+        'Owner' => array(
+            'DisplayName' => 'qcs::cam::uin/3210232098:uin/3210232098',
+            'ID' => 'qcs::cam::uin/3210232098:uin/3210232098',
+        ),));
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+
+#getObjectACL
+try {
+    $result = $cosClient->getObjectAcl(array(
+        'Bucket' => 'testbucket-1252448703',
+        'Key' => '11'));
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+
+#putBucketLifecycle
+try {
+    $result = $cosClient->putBucketLifecycle(array(
+        // Bucket is required
+        'Bucket' => 'lewzylu06-1252448703',
+        // Rules is required
+        'Rules' => array(
+            array(
+                'Expiration' => array(
+                    'Days' => 1000,
+                ),
+                'ID' => 'id1',
+                'Filter' => array(
+                    'Prefix' => 'documents/'
+                ),
+                // Status is required
+                'Status' => 'Enabled',
+                'Transitions' => array(
+                    array(
+                        'Days' => 200,
+                        'StorageClass' => 'NEARLINE'),
+                ),
+                // ... repeated
+            ),
+        )));
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+
+#getBucketLifecycle
+try {
+    $result = $cosClient->getBucketLifecycle(array(
+        // Bucket is required
+        'Bucket' =>'testbucket-1252448703',
+    ));
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+
+#deleteBucketLifecycle
+try {
+    $result = $cosClient->deleteBucketLifecycle(array(
+        // Bucket is required
+        'Bucket' =>'testbucket-1252448703',
+    ));
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+
+#putBucketCors
+try {
+    $result = $cosClient->putBucketCors(array(
+        // Bucket is required
+        'Bucket' => 'testbucket-1252448703',
+        // CORSRules is required
+        'CORSRules' => array(
+            array(
+                'ID' => '1234',
+                'AllowedHeaders' => array('*'),
+                // AllowedMethods is required
+                'AllowedMethods' => array('PUT'),
+                // AllowedOrigins is required
+                'AllowedOrigins' => array('http://www.qq.com', ),
+            ),
+            // ... repeated
+        ),
+    ));
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+
+#getBucketCors
+try {
+    $result = $cosClient->getBucketCors(array(
+        // Bucket is required
+        'Bucket' => 'testbucket-1252448703',
+    ));
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+
+#deleteBucketCors
+try {
+    $result = $cosClient->deleteBucketCors(array(
+        // Bucket is required
+        'Bucket' => 'testbucket-1252448703',
+    ));
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+
+#copyobject
+try {
+    $result = $cosClient->copyObject(array(
+        // Bucket is required
+        'Bucket' => 'lewzylu01-1252448703',
+        // CopySource is required
+        'CopySource' => 'lewzylu02-1252448703.cos.ap-guangzhou.myqcloud.com/test1G?versionId=MTg0NDY3NDI1NTk0MzUwNDQ1OTg',
+        // Key is required
+        'Key' => 'string',
+        'ServerSideEncryption' => 'AES256'
+    ));
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
+
+#Copy
+try {
+    $result = $cosClient->Copy($bucket = 'lewzylu01-1252448703',
+        $key = 'string',
+        $copysource = 'lewzylu02-1252448703.cos.ap-guangzhou.myqcloud.com/test1G',
+        $options = array('VersionId'=>'MTg0NDY3NDI1NTk0MzUwNDQ1OTg',
+            'ServerSideEncryption' => 'AES256'));
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+#restoreObject
+try {
+    $result = $cosClient->restoreObject(array(
+        // Bucket is required
+        'Bucket' => 'lewzylu02',
+        // Objects is required
+        'Key' => '11',
+        'Days' => 7,
+        'CASJobParameters' => array(
+            'Tier' =>'Bulk'
+        )
+    ));
+    print_r($result);
+} catch (\Exception $e) {
+    echo "$e\n";
+}
+
